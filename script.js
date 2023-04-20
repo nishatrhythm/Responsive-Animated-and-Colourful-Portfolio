938d950501722066a0145fc0ad915a0ffd45389f
nishatrhythm <66105148+nishatrhythm@users.noreply.github.com>
2023-04-20 14:34:50 +0600

    Design enhancement

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
 // scroll sections
 let sections = document.querySelectorAll('section');
