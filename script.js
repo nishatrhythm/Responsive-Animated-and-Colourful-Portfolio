be1f7825aab3219d91a4811d5f85f0db4238f757
nishatrhythm <66105148+nishatrhythm@users.noreply.github.com>
2023-04-20 14:39:32 +0600

    Design enhancement

--- script.js
+++ script.js
@@ -31,4 +31,8 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
