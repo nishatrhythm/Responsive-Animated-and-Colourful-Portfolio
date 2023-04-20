b70725193c7bddd2d5a49fe7b434a761f7b579b9
nishatrhythm <66105148+nishatrhythm@users.noreply.github.com>
2023-04-20 17:19:46 +0600

    Design enhancement

--- script.js
+++ script.js
@@ -41,4 +41,9 @@ window.onscroll = () => {
     // remove toggle icon and navbar when click navbar links (scroll)
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
+
+    // animation footer on scroll
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
